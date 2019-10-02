b494cab322d1729a902f170dec36274219621e5f
VMc <akusov.vladimir@gmail.com>
2019-10-03 01:09:49 +0300

    Добавил запросы SELECT UPDATE DELETE

--- Skripts/Update-01.sql
+++ Skripts/Update-01.sql
@@ -5,19 +5,25 @@ SELECT * FROM Products; --отобразить все продукты
 UPDATE --обновить
 Products -- в таблице
 SET Title = N'BALTIKA-999' --что сделать с полями
-WHERE ID = 1;
-
+WHERE Id = 1;
 
 UPDATE Products SET Title = N'Пиво';
 
 UPDATE Products SET Title = N'BALTIKA-999'
 WHERE Sku = '123000001';
+
 UPDATE Products SET Title = N'LVIVSKE'
 WHERE Sku = '123000002';
-/*
-UPDATE Products SET Quantity = Quantity + 1
-WHERE Sku = '123000002';
 
-UPDATE Products SET Quantity = 3
-WHERE Quantity = 0;
-*/
+UPDATE Sale SET Quantity = Quantity + 1
+WHERE ProductId = '2';
+
+UPDATE Sale SET Quantity = 3
+WHERE Quantity > 30;
+
+UPDATE Sale SET PriceSale = 30.12
+WHERE PriceSale > 100.01;
+
+SELECT * FROM Products;
+
+SELECT * FROM Sale;
